333011172c5c1bf176b920320a83de1a7381e807
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-04-22 21:12:14 +0000

    loops

--- code-along/5-loops.rb
+++ code-along/5-loops.rb
@@ -3,14 +3,22 @@
 # ruby 5-loops.rb
 
 # Infinite Tacos -> Finite Tacos
-index = 0
-loop do
-    if index == 5
-        break
-    end
-  puts "tacos!" 
-index = index +1
-end
+# number_of_times = 0
+# loop do
+#     if number_of_times == 5
+#         break
+#     end
+#   puts "tacos!" 
+# number_of_times = number_of_times +1
+# end
 
 # Loop through tacos
-# tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+tacos = ["carnitas", "carne asada", "pollo", "pescado"]
+number_of_times = 0
+loop do
+    if number_of_times == tacos.count
+        break
+    end
+    puts tacos[number_of_times] 
+    number_of_times = number_of_times +1
+end 
