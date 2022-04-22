f25e16a8e0f5e0dca5f91567b8b1c4bdcd3d3205
kate-hildebrand <kate.hildebrand@kellogg.northwestern.edu>
2022-04-22 19:23:16 +0000

    Mid-way check in

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,19 +1,30 @@
 # To run this code, be sure your current working directory
 # is the same as where this file is located and then run:
 # ruby 1-data.rb
+# puts "Hello, world" - remove the "#" to activate the put-s
 
 # In Ruby, there are different *types* of data:
 
 # Numbers
+#puts 7.77
 
 # Perform simple math with numbers
+# puts 5 + 2
 
 # Strings
+#puts "tacos"
 
 # Combine strings together
+#puts "#{3+3+3} tacos"
 
 # Variables
+quantity = 3+3+3
+food = "tacos"
 
 # Combine strings and variables
+#puts "#{quantity} #{food}"
 
 # String manipulation
+#puts "Hello".upcase
+#puts "Hello".reverse
+#puts "Hello".swapcase
--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,70 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+# this_is_true = true
+# puts this_is_true
+# this_is_false = false
+# puts this_is_false
 
 # Boolean Expressions
+# booleans == is it true? 
+# puts 3 == 2
+    # since this is not true, it will give us false
+# is 3 not equal to 2? 
+# puts 3 != 2
+# puts 3 > 2 
+# puts 3 < 2
 
 # If Conditional Logic
+#if ___
+    # do something 
+#end
+
+# condition will not occur
+# if 3 == 2 
+#     puts "hello!"
+# end 
+
+# if 3 > 2
+#     puts "3 is > 2!"
+# end
+
+user_entered_password = "tacos"
+real_password ="secret"
+
+# if user_entered_password == real_password
+#     puts "You're in"
+# else    
+#     puts "Login failed"
+# end
+
+# if user_entered_password != real_password
+#         puts "Login failed"
+# end
 
 # If/Else Conditional Logic
 
-# Elsif Conditional Logic
+
+
+
+# # Elsif Conditional Logic
+# your_team_score = 3
+# other_team_score = 3
+
+# if your_team_score > other_team_score
+#     puts "You Win!"
+# elsif your_team_score == other_team_score
+#     puts "You tied."
+# else 
+#     puts "You Lose!"
+#  end
+
+
 
 # Combining Expressions
+# temp = 68
+# precip = 0
+
+# if temp >= 65 && temp <= 90 || precip == 0
+#         puts "It's nice outside!"
+# end
